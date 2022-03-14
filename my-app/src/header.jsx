5933739491e8758bfa8332d0b282d32ec58d9333
bi zi <thebizi15@gmail.com>
2022-03-14 10:16:10 +0300

    Version 0.0.1

---
+++ my-app/src/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="menu">
+        <div className="choice">Все игры</div>
+        <div className="choice">Новинки</div>
+        <div className="choice">Категории</div>
+        <div className="choice">Корзина</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
