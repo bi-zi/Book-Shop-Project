50b7b0d5273f3097c8e010689ecaa52bcd60c3cd
bi zi <thebizi15@gmail.com>
2022-08-27 17:31:33 +0300

    fetchBooks update

--- src/store/actions/book.ts
+++ src/store/actions/book.ts
@@ -6,12 +6,12 @@ export const fetchBooks = () => {
   return async (dispatch: Dispatch<BookAction>) => {
     try {
       dispatch({ type: BookActionTypes.FETCH_BOOKS });
-      const response = await axios.get('https://data-base-for-app.herokuapp.com/books');
+      const response = await axios.get('https://backend-for-books-shop.herokuapp.com/books');
       dispatch({ type: BookActionTypes.FETCH_BOOKS_SUCCESS, payload: response.data });
     } catch (e) {
       dispatch({
         type: BookActionTypes.FETCH_BOOKS_ERROR,
-        payload: 'Произошла ошибка при загрузке данных',
+        payload: 'Произошла ошибка при загрузке данных (скорее всего лагает heroku)',
       });
     }
   };
