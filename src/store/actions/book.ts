8ac2bd0638ff4f6bdee69bdd0d355a00e2e343b6
bi zi <thebizi15@gmail.com>
2022-09-01 14:36:44 +0300

    minor update

--- src/store/actions/book.ts
+++ src/store/actions/book.ts
@@ -6,12 +6,12 @@ export const fetchBooks = () => {
   return async (dispatch: Dispatch<BookAction>) => {
     try {
       dispatch({ type: BookActionTypes.FETCH_BOOKS });
-      const response = await axios.get('https://backend-for-book-shop.onrender.com//books');
+      const response = await axios.get('https://backend-for-book-shop.onrender.com/books');
       dispatch({ type: BookActionTypes.FETCH_BOOKS_SUCCESS, payload: response.data });
     } catch (e) {
       dispatch({
         type: BookActionTypes.FETCH_BOOKS_ERROR,
-        payload: 'Произошла ошибка при загрузке данных (скорее всего лагает heroku)',
+        payload: 'Произошла ошибка при загрузке данных (скорее всего лагает хостинг)',
       });
     }
   };
