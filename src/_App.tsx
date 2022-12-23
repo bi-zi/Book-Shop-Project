954357e44dbc4480318a147131e5c0b47a12c75e
bi_zi <thebizi15@gmail.com>
2022-12-23 09:32:47 +0300

    trying to change the case of filenames on github

--- src/_App.tsx
+++ src/_App.tsx
@@ -1,11 +1,12 @@
-import { Layout } from './components/layout';
+import { Layout } from './components';
 import { MainPage, CategoriesPage, BookPage, BasketPage, NotFoundPage } from './pages/index';
 import { Routes, Route } from 'react-router-dom';
+import './index.css';
 
 export const App = () => {
   return (
-    <Routes >
-      <Route path="/" element={<Layout />} >
+    <Routes>
+      <Route path="/" element={<Layout />}>
         <Route index element={<MainPage />} />,
         <Route path="Category/:name" element={<CategoriesPage />} />,
         <Route path="Book/:id" element={<BookPage />} />,
